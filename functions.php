33a3f70bae0b8c4321c3883bca673c87b12693e8
Pat Eason <patrick@pateason.com>
2014-03-05 10:59:08 -0500

    CPT Function
    Function for CPT and admin columns set

--- functions.php
+++ functions.php
@@ -76,4 +76,84 @@ register_nav_menus(array(
 	'social-nav' => 'Social navigation to be used in the footer, utility, wherever'
 ));
 
+
+//Paintings CPT
+add_action( 'init', 'work_init' );
+function work_init() {
+	$labels = array(
+		'name'               => _x( 'Work' ),
+		'singular_name'      => _x( 'Work'),
+		'menu_name'          => _x( 'Work' ),
+		'name_admin_bar'     => _x( 'Work' ),
+		'add_new'            => _x( 'Add New' ),
+		'add_new_item'       => __( 'Add New Work' ),
+		'new_item'           => __( 'New Work' ),
+		'edit_item'          => __( 'Edit Work' ),
+		'view_item'          => __( 'View Work' ),
+		'all_items'          => __( 'All Work' ),
+		'search_items'       => __( 'Search Work' ),
+		'parent_item_colon'  => __( 'Parent Work:' ),
+		'not_found'          => __( 'No work found.' ),
+		'not_found_in_trash' => __( 'No work found in Trash.' ),
+	);
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'work' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => array( 'title', 'slug', 'thumbnail' )
+	);
+	register_post_type( 'work', $args );
+}
+
+
+add_filter( 'manage_edit-work_columns', 'set_custom_edit_work_columns' );
+add_action( 'manage_work_posts_custom_column' , 'custom_work_column', 10, 2 );
+
+function set_custom_edit_work_columns($columns) {
+	unset( $columns['date'] );
+
+    $columns['work_year'] = __( 'Year');
+    $columns['work_medium'] = __( 'Medium');
+    $columns['work_dimensions'] = __( 'Dimensions');
+    $columns['work_additional'] = __( 'Additional');
+    $columns['work_preview'] = __( 'Work');
+    return $columns;
+}
+
+function custom_work_column( $column, $post_id ) {
+    switch ( $column ) {
+        case 'work_preview' :
+            $img = get_the_post_thumbnail($post_id, array(150,150));
+            echo $img;
+        break;
+
+        case 'work_year' :
+            $meta = get_post_meta($post_id, 'year', true);
+            echo $meta;
+        break;
+
+        case 'work_medium' :
+            $meta = get_post_meta($post_id, 'medium', true);
+            echo $meta;
+        break;
+
+        case 'work_dimensions' :
+            $meta = get_post_meta($post_id, 'dimensions', true);
+            echo $meta;
+        break;
+
+        case 'work_additional' :
+            $meta = get_post_meta($post_id, 'additional', true);
+            echo $meta;
+        break;
+    }
+}
 ?>
